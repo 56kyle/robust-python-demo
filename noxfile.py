7dafc611b447d6b7cc6727e661a747e221d69b66
Kyle Oliver <oliverke@mail.uc.edu>
2025-07-05 14:41:58 -0400

    chore: sync up with straight project generation to remove artifacts from commit hooks when meaning to just do cruft update previously

--- noxfile.py
+++ noxfile.py
@@ -124,7 +124,7 @@ def tests_python(session: Session) -> None:
         "--cov-report=term",
         "--cov-report=xml",
         f"--junitxml={junitxml_file}",
-        "tests/",
+        "tests/"
     )
 
 
